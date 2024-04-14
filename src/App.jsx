5b36468b581d8df1d14dcef9e7d96c5f948d2a9d
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-04-14 19:23:12 -0400

    Refactor App component to use separate components for Header, HomePage, and Footer, and update layout with react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import "./App.css";
+// import "./App.css";
 import HomePage from "./components/HomePage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Col, Container, Row } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <HomePage />
+      <Container className="border">
+        <Row>
+          <Col>
+            <HomePage />
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </div>
   );
