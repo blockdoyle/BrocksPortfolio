1b300d8ccfdaa47f96ec1ec0926efcae87fbf8a6
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-04-14 19:24:26 -0400

    Update App.jsx to add border-3 class to Container component

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Container className="border">
+      <Container className="border border-3">
         <Row>
           <Col>
             <HomePage />
