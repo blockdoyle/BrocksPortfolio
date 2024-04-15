4f964e8b0beb557905f475b74b9fb5486e9b9708
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-04-15 15:04:22 -0400

    Update AboutPage content and styling

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,37 @@
 export default function AboutMe() {
   return (
     <div>
-      <h1>About Me</h1>
-      <p>
-        I am a software developer with a passion for creating and learning. I
-        have a background in customer service and a degree in computer science.
-        I am a quick learner and a team player. I am looking for a position that
-        will allow me to grow and develop my skills as a developer.
+      <h1 className="text-center">About Me</h1>
+      <img
+        src="https://avatars.githubusercontent.com/u/26856752?v=4"
+        alt="Current photo of me"
+        align="left"
+        className="me-3"
+      />
+      <p className="fs-5">
+        I'm Brock Lockhart-Doyle, a resourceful Full Stack Developer
+        specializing in mobile-first app development. Certified in Full Stack
+        Web Development from the University Coding Boot Camp, I bring a unique
+        blend of technical expertise and creative problem-solving skills to
+        every project. With a passion for innovation and a track record of
+        successful project execution, I'm poised to drive impactful solutions in
+        the dynamic field of web development. My skills span across various
+        platforms including Windows, Mac OS, and Linux, and I have scripting
+        experience with Python, PowerShell, and Bash. My recent enrollment in
+        the University of Toronto SCS - edX Full Stack Engineer Bootcamp has
+        further enhanced my proficiency in HTML, CSS, JavaScript, React.JS,
+        Node.JS, and Express.JS, positioning me as a valuable asset in any
+        development team. Additionally, I've gained practical experience in my
+        role as a Network Analyst II at the City of Hamilton.
+      </p>
+      <p className="fs-5">
+        If you are looking for a downloadable resume, see here:{" "}
+        <a
+          href="https://drive.google.com/file/d/1BKeOBLpALr5V0cE7st0R_bniHM8-niTd/"
+          target="_blank"
+        >
+          Resume
+        </a>
       </p>
     </div>
   );
