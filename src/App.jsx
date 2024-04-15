90867a6bbae3de3a5c0e949a03bc2cabd3309d0e
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-04-14 20:14:28 -0400

    moved header to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ import { Col, Container, Row } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
+      <div className="portfolio-header">
+        <div className="portfolio-title text-center">
+          <h1>Brock Lockhart-Doyle</h1>
+        </div>
+      </div>
       <Header />
       <Container className="border border-3 content-section rounded p-2 mt-3">
         <Row>
