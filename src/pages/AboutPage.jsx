caaf3048684275f8307ba98eaf2950adde56fa22
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-04-14 20:45:33 -0400

    added sample text for about me page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+export default function AboutMe() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        I am a software developer with a passion for creating and learning. I
+        have a background in customer service and a degree in computer science.
+        I am a quick learner and a team player. I am looking for a position that
+        will allow me to grow and develop my skills as a developer.
+      </p>
+    </div>
+  );
+}
