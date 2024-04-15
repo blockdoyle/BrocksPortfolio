8b5ede0025f65079562710eea0fc0df70db58dc4
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-04-14 20:52:32 -0400

    Add GitHub link to footer in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+
 function Footer() {
   return (
-    <footer className="text-center p-5 ">
-      <p>Made with ♥ by Brock Lockhart-Doyle</p>
-    </footer>
+    <>
+      <footer className="text-center p-5 ">
+        <p>Made with ♥ by Brock Lockhart-Doyle</p>
+        <Nav.Item>
+          <Nav.Link as={Link} to="https://github.com/blockdoyle">
+            GitHub
+          </Nav.Link>
+        </Nav.Item>
+      </footer>
+    </>
   );
 }
 
